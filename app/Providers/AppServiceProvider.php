5a1b50e90873d76168ec195f9f7e002ab9a0d2c6
ranelcatubigan-stack <ranel.catubigan@gmail.com>
2026-04-22 14:46:43 +0800

    update project files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,16 +8,16 @@ class AppServiceProvider extends ServiceProvider
 * Register any application services.
 */
 public function register(): void
-    {
+{
 //
-    }
+}
 /**
 * Bootstrap any application services.
 */
 public function boot(): void
-    {
-            if (env('APP_ENV') === 'production') {
-          RL::forceScheme('https');
-        }
-    }
+{
+if (env('APP_ENV') === 'production') {
+URL::forceScheme('https');
 }
+}
+} 
